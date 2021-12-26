a8599f901e5bc150ba2592837a1a1d372adfa5fa
rizaozer <rizaoozer@gmail.com>
2021-12-26 22:02:55 +0200

    ComplementaryDNA solution

---
+++  ComplementaryDNA/src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(dnaStrand("GTAT"));
+    }
+    // "ATTGC" --> "TAACG"
+    //"GTAT" --> "CATA"
+
+    // A -> T
+    // C -> G
+
+    public static String makeComplement(String dna) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < dna.length(); i++) {
+            if(dna.charAt(i) == 'A') {
+                stringBuilder.append('T');
+            }
+            if(dna.charAt(i) == 'T') {
+                stringBuilder.append('A');
+            }
+
+            if(dna.charAt(i) == 'G') {
+                stringBuilder.append('C');
+            }
+            if(dna.charAt(i) == 'C') {
+                stringBuilder.append('G');
+            }
+        }
+        String stringdna = stringBuilder.toString();
+        return stringdna;
+    }
+}
