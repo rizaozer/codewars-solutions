f0054134c35e14a78e47b9bf391ad69dda068d70
rizaozer <rizaoozer@gmail.com>
2022-01-15 21:56:59 +0200

    create files

---
+++ validBraces/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isValid("([{}])"));
+    }
+
+    public static boolean isValid(String braces) {
+
+    }
+}
