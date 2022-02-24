5c892e6a44278d32c823aa39ea5bccf80b7d9680
rizaozer <rizaoozer@gmail.com>
2022-02-24 18:23:30 +0200

    eureka solution

--- eureka/src/main/java/Main.java
+++ eureka/src/main/java/Main.java
@@ -3,25 +3,27 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(sumDigPow(1, 100));
     }
 
     public static List<Long> sumDigPow(long a, long b) {
 
         List<Long> numbers = new ArrayList<>();
 
-        for (long i = a; i < b; i++) {
+        for (long i = a; i <= b; i++) {
 
-            if(i <= 9 && i > 0) {
-                numbers.add(i);
+            long temp = 0;
+            String tempString = Long.toString(i);
+            int length = tempString.length();
+            long number = i; // 89
+            while(number > 0) {
+                temp = (long) (temp + Math.pow(number % 10, length));
+                --length;
+                number = number / 10;
             }
 
-            String stringNumber;
-
-            while(i > 9) {
-                long temp = i % 10;
-                i = i / 10;
-                double square = Math.pow(temp, temp);
+            if(temp == i) {
+                numbers.add(i);
             }
 
         }
